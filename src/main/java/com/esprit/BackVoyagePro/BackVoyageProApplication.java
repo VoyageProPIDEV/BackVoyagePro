d8ae80136173633e80e1adeba682a89dc9b346df
JemaiRihab <rihab.jemai@esprit.tn>
2022-06-20 20:44:44 +0200

    first commit

---
+++ src/main/java/com/esprit/BackVoyagePro/BackVoyageProApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.BackVoyagePro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackVoyageProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackVoyageProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esprit/BackVoyagePro/BackVoyageProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.BackVoyagePro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackVoyageProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
